6aba64e0f63139a8306e94e3ac571805ff52ab64
Sudhanva1999 <49947142+Sudhanva1999@users.noreply.github.com>
2024-04-25 14:19:02 -0400

    changes

--- src/services/config.js
+++ src/services/config.js
@@ -1,6 +1,6 @@
 import _axios from "axios";
 
-const REACT_APP_API_URL = "http://localhost:8000";
+const REACT_APP_API_URL = "hhttps://doubtstack-production.up.railway.app";
 
 const handleRes = (res) => {
     return res;
